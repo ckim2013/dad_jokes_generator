0cc1ed97f20e338c9da4c895f6a6f30099f1075f
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2020-10-01 16:28:35 -0700

    Add more to caching

--- app/javascript/frontend/components/JokeGenerator/Disclaimer.jsx
+++ app/javascript/frontend/components/JokeGenerator/Disclaimer.jsx
@@ -7,6 +7,7 @@ const Disclaimer = () => {
         <li>These dad jokes are generated using Markov chains.</li>
         <li>The jokes will be more coherent as you raise the accuracy (1-9, default 5).</li>
         <li>You can input a cue word in the text box for a butchered joke related to the cue word.</li>
+        <li>Due to caching, you might get jokes you've already seen. Wait ~5 minutes to see a new variety.</li>
         <li>Use with caution!</li>
       </ul>
     </div>
